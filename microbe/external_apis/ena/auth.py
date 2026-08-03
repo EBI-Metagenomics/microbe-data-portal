562869e5245d658a47265b84a63b468d6d0e35d9
Sandy Rogers <sandyr@ebi.ac.uk>
2026-08-03 15:35:20 +0100

    Built site for gh-pages

--- microbe/external_apis/ena/auth.py
+++
@@ -1,8 +0,0 @@
-from requests.auth import HTTPBasicAuth
-
-from microbe.utils import microbe_config
-
-if microbe_config.ena.username:
-    ENA_AUTH = HTTPBasicAuth(microbe_config.ena.username, microbe_config.ena.password)
-else:
-    ENA_AUTH = None
--- microbe/tests/settings_unit_tests.py
+++
@@ -1,9 +0,0 @@
-from microbe.settings import *
-
-UNIT_TESTING = True
-
-MICROBE_CONFIG = MicrobeConfig(
-    _env_file=microbe_config_env,
-)
-
-del STORAGES  # disable whitenoise static file serving
